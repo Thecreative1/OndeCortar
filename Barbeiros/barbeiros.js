37bee2a6ecddb22fa24f40dc3bb0f423a8384e53
Thecreative1 <flavio.martins@gmail.com>
2025-04-10 23:52:03 +0200

    Update barbeiros.js

--- Barbeiros/barbeiros.js
+++ Barbeiros/barbeiros.js
@@ -1,5 +1,5 @@
 const barbearias = [
   { nome: "Barbearia Central", coords: [41.4415, -8.2965] },
   { nome: "Corte Fino", coords: [41.4442, -8.2910] },
-  { nome: "Barbearia Fora D`Horas", coords: [41.462816, -8.320178] }
+  { nome: "Barbearia Fora D'Horas", coords: [41.462816, -8.320178] }
 ];
