defda3cd2a324b82f04acf3d3dfc62aa865b8344
Thecreative1 <flavio.martins@gmail.com>
2025-04-11 14:49:15 +0200

    Update barbeiros.js

--- Barbeiros/barbeiros.js
+++ Barbeiros/barbeiros.js
@@ -1,20 +1,13 @@
 const barbearias = [
   {
-    nome: "Barbearia Central",
-    coords: [41.4415, -8.2965]
-  },
-  {
-    nome: "Corte Fino",
-    coords: [41.4442, -8.2910]
-  },
-  {
+
     nome: "Barbearia Fora D’Horas",
     coords: [41.462816, -8.320178],
     morada: "Rua de Santa Eulália, Guimarães, Braga 4800-098",
     telefone: "+351 964 567 455",
     website: "https://www.facebook.com/Barbeariaforadhoras",
     email: "miguelbarbeiro@gmail.com"
-  }
+  },
   {
     nome: "Treze Barbershop",
     morada: "Praceta Padre Luís Gonzaga Fonseca, Loja 23, Guimarães",
