c73a8324c179ce261b14a73b78c9bb1460da96ad
Thecreative1 <flavio.martins@gmail.com>
2025-04-11 14:43:24 +0200

    Update barbeiros.js

--- Barbeiros/barbeiros.js
+++ Barbeiros/barbeiros.js
@@ -15,4 +15,38 @@ const barbearias = [
     website: "https://www.facebook.com/Barbeariaforadhoras",
     email: "miguelbarbeiro@gmail.com"
   }
+  {
+    nome: "Treze Barbershop",
+    morada: "Praceta Padre Luís Gonzaga Fonseca, Loja 23, Guimarães",
+    telefone: "+351 253 061 842",
+    email: "info@trezebarbershop.pt",
+    website: "https://trezebarbershop.pt",
+    coords: [41.4394, -8.2957]
+  },
+  {
+    nome: "Black Rock Barbearia",
+    morada: "Largo da República do Brasil, C. Comercial São Francisco, Loja 32, Guimarães",
+    telefone: "+351 253 000 000",
+    website: "https://www.fresha.com/pt/a/black-rock-barbearia-guimaraes-largo-da-republica-do-brasil-t7wbrdac",
+    coords: [41.4421, -8.2913]
+  },
+  {
+    nome: "NewVision Barbershop",
+    morada: "Rua Teixeira de Pascoais, 452, Guimarães",
+    telefone: "+351 253 106 341",
+    instagram: "https://www.instagram.com/newvision_barbershop/",
+    coords: [41.4428, -8.2905]
+  },
+  {
+    nome: "The Barbers Studio",
+    morada: "Rua Teixeira de Pascoais, 40B, Guimarães",
+    facebook: "https://www.facebook.com/BarbeariaTheBarbersStudio/",
+    coords: [41.4425, -8.2902]
+  },
+  {
+    nome: "1920 Barber Shop",
+    morada: "GuimarãeShopping, Guimarães",
+    website: "https://www.guimaraeshopping.pt/lojas/1920-barber-shop/",
+    coords: [41.4375, -8.2910]
+  }
 ];
